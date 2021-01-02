41f4c5860f77253b7ca17671611687f1310680b0
Vaughan Po <vaughan188@gmail.com>
2021-01-03 00:37:25 +0530

    baseline code structure

---
+++ src/services/search/routes.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { getPlacesByName } from "./SearchController";
+import { checkSearchParams } from "../../middleware/checks";
+
+export default [
+  {
+    path: "/api/v1/search",
+    method: "get",
+    handler: [
+      checkSearchParams,
+      async ({ query }: Request, res: Response) => {
+        if (typeof(query.q) == 'string') {
+          const result = await getPlacesByName(query.q);
+          res.status(200).send(result);
+        }
+      }
+    ]
+  }
+];
