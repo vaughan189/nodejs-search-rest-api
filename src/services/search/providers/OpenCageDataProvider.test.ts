94d0f9720afcedb3bbbdb10f94b6384acadc4f3e
Vaughan Po <vaughan188@gmail.com>
2021-01-03 20:20:32 +0530

    unit and integration tests added

---
+++ src/services/search/providers/OpenCageDataProvider.test.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { getPlaces } from './OpenCageDataProvider';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('OpenCageDataProvider', () => {
+    test('an empty query string', async () => {
+        mockedAxios.get.mockResolvedValue({ data: { features: [] } });
+        const result = await getPlaces('Paris');
+        expect(result).toEqual({ features: [] });
+    });
+
+    test('an invalid non-json response', async () => {
+        mockedAxios.get.mockRejectedValue(new Error('Service Unavailable.'));
+        await expect(getPlaces('Chamonix')).rejects.toThrow('Service Unavailable.');
+    });
+});
